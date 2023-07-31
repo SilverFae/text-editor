20d0d54d606e8c4f2ea3d8127166060533e69755
Shannon <shogeboom187@gmail.com>
2023-07-31 08:59:17 -0700

    finished webpck config

--- Develop/client/webpack.config.js
+++ Develop/client/webpack.config.js
@@ -23,12 +23,48 @@ module.exports = () => {
         template: './index.html',
         title: 'J.A.T.E.',
       }),
-      
+      new InjectManifest({
+        swSrc: './src-sw.js',
+        swDest: 'src-sw.js',
+      }),
+      new WebpackPwaManifest({
+        fingerprints: false,
+        inject: true,
+        name: 'Just Another Text Editor',
+        short_name: 'J.A.T.E.',
+        description: 'A simple text editor that works offline.',
+        background_color: '#ffffff',
+        theme_color: '#ffffff',
+        start_url: '/',
+        puplicPath: '/',
+        icons: [
+          {
+            src: path.resolve('src/assets/icon.png'),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join('assets', 'icons'),
+          },
+        ],
+      }),
     ],
 
     module: {
       rules: [
-        
+        {
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader'],
+        },
+        {
+          test: /\.m?js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options:
+            {
+              presets: ['@babel/preset-env'],
+              plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/transform-runtime'],
+            },
+          },
+        },
       ],
     },
   };
