c0814cc0313579e57cf1c29f4e474bd992774349
Shannon <shogeboom187@gmail.com>
2023-07-31 08:49:11 -0700

    implemented asset caching

--- Develop/client/src-sw.js
+++ Develop/client/src-sw.js
@@ -27,5 +27,24 @@ warmStrategyCache({
 registerRoute(({ request }) => request.mode === 'navigate', pageCache);
 
 // TODO: Implement asset caching
+registerRoute(
+  // this is a custom function that returns true if the request is a font
+  ({ request }) => request.destination === 'script' || request.destination === 'style',
+  // Use cache but update in the background.
+  new CacheFirst({
+    cacheName: 'asset-cache',
+    plugins: [
+      // Ensure that only requests that result in a 200 status are cached
+      new CacheableResponsePlugin({
+        statuses: [0, 200],
+      }),
+      // Don't cache more than 50 items, and expire them after 30 days
+      new ExpirationPlugin({
+        maxAgeSeconds: 30 * 24 * 60 * 60,
+      }),
+    ],
+  })
+);
+
 
 registerRoute();
